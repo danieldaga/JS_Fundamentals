61829ea84817db3ac1b0ea88857679b6bc2b17ea
danieldaga <daniel@arhrestauracion.com>
2022-10-31 19:04:00 +0100

    arrays ex4

--- array/main.js
+++ array/main.js
@@ -45,17 +45,44 @@ const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
 
 //exercise 3.1
 
-function searchWed(array) {
-    array.indexOf('Wednesday')  //indice 2
-    if (array[2].length > array.indexOf('Wednesday')) {
-        array.reverse()
-    }
-    // console.log(array);
-}
-searchWed(days)
+// function searchWed(array) {
+//     if (array[2].length > array.indexOf('Wednesday')) {
+//         return array.reverse()
+//     }
+//     // console.log(array);
+// }
+// searchWed(days)
 
 //exercise 3.2
 
+function compare (array) {
+    if (array[0].length < array[1].length) {
+        return array[0,1,2]
+    }
+    console.log(array);
+}
+compare(days)
+
+//exercise 3.3
+
 function searchSunday (array) {
     
+    if (array.includes('sunday')) {
+        console.log("That's nice!")
+    }else{
+        console.log("Oh no!")
+        array.push('Sunday')
+    }
 }
+searchSunday(days)
+console.log(days);
+
+
+//exercise 4
+
+let str = ('Paralelepípedo')
+function vuelta (string) {
+    string.split(0,14)
+    console.log(str);
+}
+vuelta(str)
